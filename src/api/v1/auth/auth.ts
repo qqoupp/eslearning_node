05bd4f3a566ed76c3d5ec1617a3322109a9432d8
Emil Toader <emil.toader@toptal.com>
2023-11-19 13:15:50 +0200

    first api route

---
+++ src/api/v1/auth/auth.ts
@@ -0,0 +1,14 @@
+import exp from 'constants';
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    
+    res.status(200).json({
+        success: true,
+        message: 'Hello World!'
+    });
+  });
+
+export default router;
